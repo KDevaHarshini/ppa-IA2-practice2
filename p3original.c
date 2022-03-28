776bf9782a612fbca8d33d1e9fe2378bd3fe50a9
kancharla devaharshini <94678326+KDevaHarshini@users.noreply.github.com>
2022-03-28 17:34:31 +0000

    p3 done

--- p3original.c
+++ p3original.c
@@ -1,4 +1,4 @@
-#include <stdio.h>
+#include<stdio.h>
 int input_number()
 {
   int n;
@@ -10,12 +10,12 @@ int is_composite(int n)
 {
   int i=2;
   while(i<=n/2)
-  {
-    if(n%i==0)
-    return 1;
-    i++;
-    }  
-  return 0;   
+    {
+      if(n%i==0)
+      return 1;
+      i++;
+    }
+  return 0;
 }
 void output(int n,int is_composite)
 {
@@ -26,12 +26,12 @@ void output(int n,int is_composite)
   else
   {
     printf("The number is not composite\n");
-    }
-  }
+  }  
+}
 int main()
 {
   int n=input_number();
   int composite= is_composite(n);
   output(n,composite);
   return 0;
-  }
+}
